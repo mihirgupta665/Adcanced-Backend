522a78c7dc6de162f0e9f5bd76a48e1028d3403f
Mihir Gupta <mihirgupta665@gmail.com>
2025-12-07 21:21:42 +0530

    Creating a session ID using express-session

--- 4_Router_Cookies_Sessions/index.js
+++ 4_Router_Cookies_Sessions/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const Posts = require("./routes/posts.js");     // routes are defined for particular models
 const Users = require("./routes/users.js"); 
 const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");      // whenever we need to add a session id we need to import express-session
+
 
 const router = express.Router();
 const app = express();
@@ -10,9 +12,14 @@ app.listen(3000, ()=>{
     console.log("Listening through port 3000");
 })
 
+app.use(expressSession({secret : "MySuperSecretCode"}));    // Session ID : every page of teh website will be having now a session id
+app.use(cookieParser("secret"));
 app.use("/posts", Posts);
 app.use("/users", Users);
 app.get("/", (req, res)=>{
+    res.cookie("TestCode", "Wow", {signed : true});    // signing the coking name value pair therefore need to add middleware of cookie parser
     res.send("Home Directory");
 }); 
-
+app.get("/test", (req, res)=>{
+    res.send("Testing Successfull");
+});

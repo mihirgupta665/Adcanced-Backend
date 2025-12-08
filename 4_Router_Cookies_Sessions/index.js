f2b0402821545288c5e0e6b63dcc8c15ea5b1998
Mihir Gupta <mihirgupta665@gmail.com>
2025-12-08 11:01:48 +0530

    Creating flash message for successfull user registration

--- 4_Router_Cookies_Sessions/index.js
+++ 4_Router_Cookies_Sessions/index.js
@@ -4,7 +4,8 @@ const Posts = require("./routes/posts.js");     // routes are defined for partic
 const Users = require("./routes/users.js"); 
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");      // whenever we need to add a session id we need to import express-session
-
+const flash = require("connect-flash");     // npm i connect-flash : to flash message we need to import connect-flash npm package
+const path = require("path");
 
 const router = express.Router();
 const app = express();
@@ -12,12 +13,17 @@ app.listen(3000, ()=>{
     console.log("Listening through port 3000");
 })
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(expressSession({
     secret : "MySuperSecretCode",
     resave: false,
     saveUninitialized : true
 }));    // Session ID : every page of teh website will be having now a session id
 app.use(cookieParser("secret"));
+app.use(flash());   // the flash-connect object need to be used in application
+
 app.use("/posts", Posts);
 app.use("/users", Users);
 app.get("/", (req, res)=>{
@@ -35,6 +41,17 @@ app.get("/record", (req, res)=>{
         req.session.count=1;    // stored in memory storage (default server side temporary session storage)
     }
     res.send(`The Response is made for the ${req.session.count} times`);
+});
+
+app.get("/register", (req, res)=>{
+    let { name = "anonymous" } = req.query;
+    req.flash("success", "User Registered Successfully");   // first time flash for key need to be written written in pair of key and vlaue
+    req.session.name = name;
+    res.redirect("/greet");
+});
+
+app.get("/greet", (req, res)=>{
+    res.render("show.ejs", { name : req.session.name, msg : req.flash("success") });    // already initialized flash message could be extracted using the key
 });
 
 // default server side session temporary storage is called as memeory storage
